ba501fcfe9ad28d6de6d8f6dc175d4a30d30060d
Romain Durand <romain.durand@radiofrance.com>
2024-11-07 10:25:39 +0100

    repro commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import { hello } from '$lib/index.js';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={hello}>Hello !</button>
